c9804cae359f3eff4a85c7ad87f70f203e53c53b
JensenJ <jensenbradley@icloud.com>
2019-07-30 21:37:57 +0100

    Basic Renderer

--- src/game/MainGameLoop.java
+++ src/game/MainGameLoop.java
@@ -3,19 +3,40 @@ package game;
 import org.lwjgl.opengl.Display;
 
 import render.DisplayManager;
+import render.Loader;
+import render.RawModel;
+import render.Renderer;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		DisplayManager.CreateDisplay();
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
 		
 		while(!Display.isCloseRequested()) {
+			renderer.Prepare();
 			//Game logic
 			//Render
+			renderer.Render(model);
+			
 			
 			DisplayManager.UpdateDisplay();
 		}
 		
+		loader.CleanUp();
 		DisplayManager.CloseDisplay();
 	}
 }
