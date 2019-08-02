7409525cc0d678e7837f9f007db23ccaaf5471ab
JensenJ <jensenbradley@icloud.com>
2019-08-02 21:52:49 +0100

    Transformations
    Location, Rotation, Scale implemented for rendering

--- src/shaders/ShaderProgram.java
+++ src/shaders/ShaderProgram.java
@@ -3,25 +3,39 @@ package shaders;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.FloatBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
 
 public abstract class ShaderProgram {
 	
 	private int programID;
 	private int vertexShaderID;
 	private int fragmentShaderID;
+	
+	private static FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
 
 	public ShaderProgram(String vertexFile, String fragmentFile) {
-		vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
-		fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
+		vertexShaderID = LoadShader(vertexFile, GL20.GL_VERTEX_SHADER);
+		fragmentShaderID = LoadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
 		programID = GL20.glCreateProgram();
 		
 		GL20.glAttachShader(programID, vertexShaderID);
 		GL20.glAttachShader(programID, fragmentShaderID);
+		BindAttributes();
 		GL20.glLinkProgram(programID);
 		GL20.glValidateProgram(programID);
+		GetAllUniformLocations();
+	}
+	
+	protected abstract void GetAllUniformLocations();
+	
+	protected int GetUniformLocation(String uniformName) {
+		return GL20.glGetUniformLocation(programID, uniformName);
 	}
 	
 	public void Start() {
@@ -47,7 +61,25 @@ public abstract class ShaderProgram {
 		GL20.glBindAttribLocation(programID, attribute, variableName);
 	}
 	
-	private static int loadShader(String file, int type) {
+	protected void LoadFloat(int location, float value) {
+		GL20.glUniform1f(location, value);
+	}
+	
+	protected void LoadVector(int location, Vector3f vector) {
+		GL20.glUniform3f(location, vector.x, vector.y, vector.z);
+	}
+	
+	protected void LoadBoolean(int location, boolean value) {
+		GL20.glUniform1f(location, value ? 1 : 0);
+	}
+	
+	protected void LoadMatrix(int location, Matrix4f matrix) {
+		matrix.store(matrixBuffer);
+		matrixBuffer.flip();
+		GL20.glUniformMatrix4(location, false, matrixBuffer);
+	}
+	
+	private static int LoadShader(String file, int type) {
 		StringBuilder shaderSource = new StringBuilder();
 		
 		try {
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	
+	private int location_transformationMatrix;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -13,6 +17,15 @@ public class StaticShader extends ShaderProgram {
 	protected void BindAttributes() {
 		super.BindAttribute(0, "position");
 		super.BindAttribute(1, "textureCoords");
+	}
+
+	@Override
+	protected void GetAllUniformLocations() {
+		location_transformationMatrix = super.GetUniformLocation("transformationMatrix");
+	}
+	
+	public void LoadTransformationMatrix(Matrix4f matrix) {
+		super.LoadMatrix(location_transformationMatrix, matrix);
 	}
 
 }
